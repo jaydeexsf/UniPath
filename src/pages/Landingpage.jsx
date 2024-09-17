160044357abf571a01b31035a7013343094c16d1
johannes <mojohannes06@gmail.com>
2024-09-17 18:10:52 +0200

    a'

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -324,7 +324,7 @@ const LandingPage = () => {
 
       <footer className="bg-gray-900 text-gray-400 py-4">
         <div className="container mx-auto text-center">
-          <p>&copy; 2024 GuidanceApp. All Rights Reserved.</p>
+          <p>&copy; 2024 Guidance. All Rights Reserved.</p>
           <div className="flex justify-center mt-4">
             <FaFacebook className="mx-2" />
             <FaTwitter className="mx-2" />
